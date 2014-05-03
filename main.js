435fd64a0d823f98bce5758d4bcb9cae5523683d
Josh Horwitz <jhorwit2@gmu.edu>
2014-05-03 18:27:49 -0400

    trying yet another test

--- main.js
+++ main.js
@@ -1,11 +1,4 @@
-OWF.relayFile = '/rpc_relay.uncompressed.html';
-
-console.log(owfdojo);
-
-owfdojo.addOnLoad(function () {
-    console.log('add on load');
-    OWF.ready(init);
-});
+//OWF.relayFile = '/rpc_relay.uncompressed.html';
 
 function init() {
     console.log('init relay widget');
@@ -13,3 +6,15 @@ function init() {
     console.log('umap init');
 
 }
+
+$(document).ready(function () {
+    if (typeof OWF !== 'object' || !OWF.Util.isRunningInOWF()) { // jslint wont allow Ozone === 'undefined' and despite crockfords claim, Ozone === undefined blows up the browser here
+        alert('Map Widget tester only works as a widget in an OWF dashboard');
+        return;
+    }
+
+    OWF.ready(function () {
+        widgetId = JSON.parse(Ozone.eventing.Widget.getInstance().getWidgetId()).id;
+        init();
+    });
+});
