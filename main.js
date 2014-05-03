cc0b69f2280b7f75d8adf04aafa34b8f95b83d07
Josh Horwitz <jhorwit2@gmu.edu>
2014-05-03 17:28:27 -0400

    Update main.js for owf

--- main.js
+++ main.js
@@ -1,24 +1,7 @@
-if (OWF.Util.isRunningInOWF()) {
+owfdojo.addOnLoad(function () {
+    OWF.ready(init);
+});
 
-    // Enable logging in case we need it
-    var logger = OWF.Log.getDefaultLogger();
-    var appender = logger.getEffectiveAppenders()[0];
-    appender.setThreshold(log4javascript.Level.INFO);
-    OWF.Log.setEnabled(true);
-
-    // once OWF has loaded
-    OWF.ready(function() {
-        umap_subscribe();
-
-        // launch common map widget if it's not already running
-        OWF.Launcher.launch({
-            universalName: 'org.jc2cui.mapWidget',
-            launchOnlyIfClosed: true,
-            data: ""
-        }, function (result) {
-            OWF.Eventing.subscribe("hackathon.scenario.broadcast", deployScenarioPhase);
-            setInterval(plotDynamic, 2000);
-            OWF.notifyWidgetReady();
-        });
-    });
+function init() {
+    umap_subscribe();
 }
