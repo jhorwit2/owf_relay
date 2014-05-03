03cfd4f62b94dd8a1f795a2e0474140c73c2adbb
Josh Horwitz <jhorwit2@gmu.edu>
2014-05-03 19:44:36 -0400

    fixed paran bug

--- eventing.js
+++ eventing.js
@@ -40,7 +40,7 @@
 
         function onMessage(sender_uid, message, channel) {
             sender_uid = $.parseJSON(sender_uid);
-            if (_(sender_uid).isEqual(OWF.getInstanceUID()) {
+            if (_(sender_uid).isEqual(OWF.getInstanceUID())) {
                 return;
             }
 
