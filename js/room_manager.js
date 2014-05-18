968a8d18d29bc0562aac3d11095d889f46f11df3
Josh Horwitz <jhorwit2@gmu.edu>
2014-05-18 14:03:49 -0400

    Revert

--- js/room_manager.js
+++ js/room_manager.js
@@ -1,27 +1,38 @@
+var rooms = [];
+var users = [];
+
 function loadRooms() {
-    $('#room-list').html("");
     try {
-        window.relay.getCurrentRoom( function (currentRoom) {
-            addRoom(currentRoom);
-            $('#btn_' + currentRoom).addClass('current');
-            window.relay.getAllRooms(function (allRooms) {
-                for (i in allRooms) {
-                    if (allRooms[i] !== currentRoom) {
-                        addRoom(allRooms[i]);
-                    }
-                }
-            });
-            getUsers(currentRoom);
+        window.relay.getAllRooms(function (allRooms) {
+            var cr;
+            if ($(rooms).not(allRooms).length !== 0 || $(allRooms).not(rooms).length !== 0) { //if array don't contain same elements
+                $('#room-list').html("");
+                rooms = [];
+                window.relay.getCurrentRoom( function (currentRoom) {
+                    addRoom(currentRoom);
+                    $('#btn_' + currentRoom).addClass('current');
+                    rooms.push(currentRoom);
+					_(allRooms).each(function (room) {
+						if (room !== currentRoom) {
+							addRoom(room);
+							rooms.push(room);
+						}
+					});
+                    cr = currentRoom;
+                });
+            }
+            getUsers(cr);
         });
     } catch(err) {
         console.log(err);
     }
 }
+
 function addRoom(room) {
     $('#room-list').append(
         $('<li>').attr({
-            'class': 'room-btn col-sm-4',
-            'id': 'btn_' + room
+        	'class': 'room-btn col-sm-4',
+        	'id': 'btn_' + room
         }).append(
             $('<a>').attr('href','#').append(room)
         )
@@ -32,20 +43,29 @@ function addRoom(room) {
         loadRooms();
     });
 }
+
 function getUsers(room) {
-    $('#room-users').html("");
-    window.relay.getUsername(function (currentUser) {
-        addUser(currentUser);
-        $('#user_' + currentUser).addClass('current');
-        window.relay.getAllUsers(function (allUsers) {
-            for (i in allUsers) {
-                if (allUsers[i] !== currentUser) {
-                    addUser(allUsers[i]);
-                }
-            }
-        });
+    window.relay.getAllUsers(function (allUsers) {
+        var cu;
+        if ($(users).not(allUsers).length !== 0 || $(allUsers).not(users).length !== 0) { //if array don't contain same elements
+            $('#room-users').html("");
+            users = [];
+            window.relay.getUsername(function (currentUser) {
+                addUser(currentUser);
+                $('#user_' + currentUser).addClass('current');
+                users.push(currentUser);
+                _(allUsers).each(function (user) {
+					if (user !== currentUser) {
+						addUser(user);
+						users.push(user);
+					}
+				});
+                cu = currentUser;
+            });
+        }
     });
 }
+
 function addUser(user) {
     $('#room-users').append(
         $('<li>').attr({
@@ -58,29 +78,27 @@ function addUser(user) {
 }
 
 $(document).ready(function () {
-    $(window).bind('relayLoaded', function(e) {
+    $(window).bind('relayLoaded', function (e) {
         loadRooms();
-        var refreshCount = setInterval( function() { //reload users every 10 sec
-            loadRooms();
-        }, 10000);
+        var refreshCount = setInterval(loadRooms, 5000);
 
-        $('#roomNameSubmit').click( function () {
+        $('#roomNameSubmit').click(function () {
             roomName = $('#roomName').val();
             if (roomName !== "") {
-                $('#roomName').val("")
+                $('#roomName').val("");
                 relay.switchRoom(roomName);
                 loadRooms();
             }
         });
-        $('#userNameSubmit').click( function () {
+        $('#userNameSubmit').click(function () {
             userName = $('#userName').val();
             if (userName !== "") {
-                $('#userName').val("")
+                $('#userName').val("");
                 relay.setUsername(userName);
                 $('#room-users .current a').html(userName);
             }
         });
-        $(window).keydown(function(event){
+        $(window).keydown(function (event){
             if(event.keyCode == 13) {  //if enter key is clicked
                 event.preventDefault();
                 if ($('#roomName').is(':focus')) {
